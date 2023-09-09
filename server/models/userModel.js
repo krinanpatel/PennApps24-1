9fbb614ca3b2ebdb02882a17b189c06a4abd7e74
Timothy Tuen <timothytuen45@gmail.com>
2023-09-09 16:39:05 -0400

    Passport

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,2 +1,26 @@
 import mongoose from "mongoose";
 import passportLocalMongoose from "passport-local-mongoose";
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: String
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: false
+    }
+})
+
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", userSchema);
+export default User;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import session from "express-session";
@@ -19,13 +20,57 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
     cookie: {
-        // sameSite: "none",
-        // secure: true,
         maxAge: 24*60*60*1000
     }
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+
+// console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("mongoDB connected successfully")
+}).catch(err => {
+    console.log(err);
+})
+
+import User from "./models/userModel.js";
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.post("/api/newUser", (req, res) => {
+    console.log("new user!")
+    console.log(req.body);
+    const email = req.body.email;
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const number = req.body.mobileNumber;
+
+    const newUser = new User({
+        username: email,
+        number: number,
+        firstName: fName,
+        lastName: lName
+    })
+
+    User.register(newUser, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            console.log("couldn't register")
+            res.redirect("/");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/news");
+            })
+        }
+    })
+})
+
+app.post("/api/login", passport.authenticate('local', {failureMessage: true}), (req, res) => {
+    console.log("successful authentication/login")
+    console.log(req.user)
+    res.send(req.user);
+})
 
 
 app.get("/api/home", (req, res) => {
