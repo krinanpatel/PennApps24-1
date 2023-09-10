dca576b66b12b961fc948bce0a7958d5dd83feb0
krinanpatel <110437367+krinanpatel@users.noreply.github.com>
2023-09-09 20:39:07 -0400

    Update news.js

--- PennApps24-master/client/pages/news.js
+++ PennApps24-master/client/pages/news.js
@@ -1,23 +1,41 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react';
 
-function article(title, link, date) {
-    return (
-        <div className='grid grid-cols-3 text-sm'>
-            <p>{title}</p>
-            <p>{link}</p>
-            <p>{date}</p>
-        </div>
-    )
+function App() {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    backgroundColor: 'lightblue',
+    padding: '20px',
+    borderRadius: '10px',
+  };
+
+  const boxStyle = {
+    textAlign: 'center',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '10px',
+    backgroundColor: 'white',
+    marginBottom: '20px', 
+    maxWidth: '50%', 
+  };
+
+  return (
+    
+    <div style={containerStyle}>
+      <div style={boxStyle}>
+        <h1><b>News Customized Just for You! </b></h1>
+      </div>
+      <div style={boxStyle}>
+        <h1>Top Box</h1>
+        <p><b>This is the top box with some text for the name.</b></p>
+      </div>
+     
+      <div style={boxStyle}>
+        <p><b>This is the bottom box with a few sentences worth of text. It will be a small description of the article.</b></p>
+      </div>
+    </div>
+  );
 }
 
-function news() {
-    const articleData = axios.get("/api/getArticles")
-
-    return (
-        <div>
-
-        </div>
-    )
-}
-
-export default news
+export default App;
